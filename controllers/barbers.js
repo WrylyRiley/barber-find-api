364b08955bbb8e791fd0314cd449300985baff5a
Christopher Tran <christopher.long.tran@gmail.com>
2018-01-29 12:40:40 -0500

    created and modified model, and seed data, as well as created the rest of the connection functions

--- controllers/barbers.js
+++ controllers/barbers.js
@@ -1,12 +1,42 @@
 const express = require('express')
 const router = express.Router()
 
-const mongoose = require('../db/schema')
-const Barbers = mongoose.modle('Barber')
+const mongoose = require('../db/models/barber')
+const Barbers = mongoose.model('Barber')
 
 router.get('/', (req, res) => {
   Barbers.find()
     .then(barber => res.json(barber))
+    .catch(err => console.log(err))
+})
+
+router.post('/', (req, res) => {
+  Barbers.create(req.body)
+    .then(barber => {
+      res.json(barber)
+    })
+    .catch(err => console.log(err))
+})
+
+router.get('/:id', (req, res) => {
+  Barbers.findById(req.params.id)
+    .then(barber => res.json(barber))
+    .catch(err => console.log(err))
+})
+
+router.put('/:id', (req, res) => {
+  Barbers.findOneAndUpdate({_id: req.params.id}, req.body, {new: true})
+    .then(barber => {
+      res.json(barber)
+    })
+    .catch(err => console.log(err))
+})
+
+router.delete('./id', (req, res) => {
+  Barbers.findOneAndRemove({_id: req.params.id})
+    .then(work => {
+      res.json(work)
+    })
     .catch(err => console.log(err))
 })
 
--- db/connection.js
+++ db/connection.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = Promise
 
-
 // // Dev
 // mongoose.connect('mongodb://localhost/posts', { useMongoClient: true })
 
 // Prod
-if (process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV === 'production') {
   mongoose.connect(process.env.MLAB_URL, { useMongoClient: true })
 } else {
   mongoose.connect('mongodb://localhost/posts', { useMongoClient: true })
