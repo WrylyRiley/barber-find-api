09230929d4c3754bb323075739fce33d99dfb0a8
Zach Bauer <zbauer91@gmail.com>
2018-01-30 18:49:45 -0500

    added a few console logs

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 const Barber = require('../db/schema')
-
-const request = require('request')
-
+const request = require('request-promise-native')
 const numItems = 3
+const chalk = require('chalk')
 
-request(
+request
+  .get(
   {
     url: 'https://api.foursquare.com/v2/venues/explore',
     method: 'GET',
@@ -17,49 +17,64 @@ request(
       limit: numItems
     }
   },
-  (err, body) => {
-    if (err) {
-      console.error(err)
-    } else {
+    (err, res, body) => {
       let seedData = []
-      let data = JSON.parse(body)
+      if (err) {
+        console.error(err)
+      } else {
+        let data = JSON.parse(body)
+        for (let i = 0; i < numItems; i++) {
+          let returned = data.response.groups[0]
+          let items = returned.items[i]
 
-      for (let i = 0; i < numItems; i++) {
-        let returned = data.response.groups[0]
-        let items = returned.items[i]
+          // console.log(venueID)
 
-        // console.log(venueID)
+          let venueID = items.venue.id
+          let name = items.venue.name
+          let address = items.venue.location.address
+          let rating = items.venue.rating
+          let website = items.url
+          let postalcode = items.venue.location.postalCode
+          let hours = items.venue.hours.status || 'None Listed'
+          let phone = items.venue.contact.formattedPhone
+          let city = items.venue.location.city
+          let state = items.venue.location.state
 
-        let venueID = items.venue.id
-        let name = items.venue.name
-        let address = items.venue.location.address
-        let rating = items.venue.rating
-        let website = items.url
-        let postalcode = items.venue.location.postalCode
-        let hours = items.venue.hours.status || 'None Listed'
-        let phone = items.venue.contact.formattedPhone
-        let city = items.venue.location.city
-        let state = items.venue.location.state
+          console.log(chalk.red(items.venue.id))
+          console.log(chalk.red(items.venue.name))
+          console.log(chalk.red(items.venue.location.address))
+          console.log(chalk.red(items.venue.rating))
+          console.log(chalk.red(items.venue.url))
+          console.log(chalk.red(items.venue.location.postalCode))
+          console.log(chalk.red(items.venue.hours.status))
+          console.log(chalk.red(items.venue.contact.formattedPhone))
+          console.log(chalk.red(items.venue.location.city))
+          console.log(chalk.red(items.venue.location.state))
 
-        seedData.push({
-          venueID,
-          name,
-          address,
-          rating,
-          website,
-          postalcode,
-          hours,
-          phone,
-          city,
-          state
-        })
+          seedData.push({
+            venueID: venueID,
+            name: name,
+            address: address,
+            rating: rating,
+            website: website,
+            postalcode: postalcode,
+            hours: hours,
+            phone: phone,
+            city: city,
+            state: state
+          })
+          console.log(chalk.yellow(seedData))
+        }
       }
+      console.log(chalk.blue(seedData))
+      return seedData
     }
-    return seedData
-  }
-)
+  )
   .then(seedData => {
+    console.log(chalk.blue(seedData))
     seedData.forEach((item, idx) => {
+      console.log(chalk.red(item))
+      console.log(chalk.green(idx))
       request(
         {
           url: 'https://api.foursquare.com/v2/venues/VENUE_ID/tips',
