f7131bc54e6d964a0ec39f8dac2b0e14b434d81b
Christopher Tran <christopher.long.tran@gmail.com>
2018-01-30 18:33:04 -0500

    uncommented out MLAB connection

--- db/connection.js
+++ db/connection.js
@@ -5,11 +5,11 @@ mongoose.Promise = Promise
 // Dev
 mongoose.connect('mongodb://localhost/barberhub', { useMongoClient: true })
 
-// // Prod
-// if (process.env.NODE_ENV == 'production') {
-//   mongoose.connect(process.env.MLAB_URL, { useMongoClient: true })
-// } else {
-//   mongoose.connect('mongodb://localhost/barberhub', { useMongoClient: true })
-// }
+// Prod
+if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(process.env.MLAB_URL, { useMongoClient: true })
+} else {
+  mongoose.connect('mongodb://localhost/barberhub', { useMongoClient: true })
+}
 
 module.exports = mongoose
